4744727558af499356098da97133397b52bd7b7f
Toqa <tokamohsen@outlook.com>
2018-05-01 21:02:32 +0200

    add rain

--- Assets/Let's Try Shooter/Scripts/CompletedScripts/RayCastShootComplete.cs
+++ Assets/Let's Try Shooter/Scripts/CompletedScripts/RayCastShootComplete.cs
@@ -7,12 +7,16 @@ public class RayCastShootComplete : MonoBehaviour {
 	public float fireRate = 0.25f;										// Number in seconds which controls how often the player can fire
 	public float weaponRange = 50f;										// Distance in Unity units over which the player can fire
 	public float hitForce = 100f;										// Amount of force which will be added to objects with a rigidbody shot by the player
-	public Transform gunEnd;											// Holds a reference to the gun end object, marking the muzzle location of the gun
+	public Transform gunEnd;
+	public  GameObject GunParticle;
+	// Holds a reference to the gun end object, marking the muzzle location of the gun
 
 	private Camera fpsCam;												// Holds a reference to the first person camera
 	private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);	// WaitForSeconds object used by our ShotEffect coroutine, determines time laser line will remain visible
 	private AudioSource gunAudio;										// Reference to the audio source which will play our shooting sound effect
-	private LineRenderer laserLine;										// Reference to the LineRenderer component which will display our laserline
+	private LineRenderer laserLine;	
+
+	// Reference to the LineRenderer component which will display our laserline
 	private float nextFire;												// Float to store the time the player will be allowed to fire again, after firing
 
 	public Ball ballscript;
@@ -48,7 +52,9 @@ public class RayCastShootComplete : MonoBehaviour {
 
 			// Set the start position for our visual effect for our laser to the position of gunEnd
 			laserLine.SetPosition (0, gunEnd.position);
-
+			 Instantiate(GunParticle, this.transform.position, this.transform.rotation);
+			//Instantiate(GunParticle , );
+			//GunParticle.
 			// Check if our raycast has hit anything
 			if (Physics.Raycast (rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
 			{
